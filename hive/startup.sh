c5535925eba254aad08f5d0d67a426980c87a4a8
twan <18330156177@163.com>
2023-10-15 20:20:06 +0800

    hive3.1.3配置

--- hive/startup.sh
+++ hive/startup.sh
@@ -5,6 +5,4 @@ hadoop fs -mkdir -p    /user/hive/warehouse
 hadoop fs -chmod g+w   /tmp
 hadoop fs -chmod g+w   /user/hive/warehouse
 hiveservices.sh start
-
-cd $HIVE_HOME/bin
-./hiveserver2 --hiveconf hive.server2.enable.doAs=false
+hiveserver2 --hiveconf hive.server2.enable.doAs=false
