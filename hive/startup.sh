9a229da1a41e396197dfd03756bf2ee40b92814a
twan <18330156177@163.com>
2023-10-15 21:13:41 +0800

    hive3.1.3配置

--- hive/startup.sh
+++ hive/startup.sh
@@ -4,5 +4,6 @@ hadoop fs -mkdir       /tmp
 hadoop fs -mkdir -p    /user/hive/warehouse
 hadoop fs -chmod g+w   /tmp
 hadoop fs -chmod g+w   /user/hive/warehouse
+schematool -dbType mysql -initSchema -verbose
 hiveservices.sh start
 hiveserver2 --hiveconf hive.server2.enable.doAs=false
