a483af8f743e34c508373dd0b4c0c19bcdba785c
twan <18330156177@163.com>
2023-10-27 21:51:01 +0800

    hive3 编码配置

--- hive/startup.sh
+++ hive/startup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+cd $HIVE_HOME/scripts/metastore/upgrade/mysql/
+#sed -i 's/CHARSET=latin1/CHARSET=utf8/g' *.sql
+#sed -i 's/latin1_bin/utf8_general_ci/g' *.sql
+#sed -i 's/latin1/utf8/g' *.sql
 
 hadoop fs -mkdir       /tmp
 hadoop fs -mkdir -p    /user/hive/warehouse
