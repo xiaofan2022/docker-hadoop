94a222b5cf3d52ad73f7157665af67eb501a571d
wantao <wantao@inmyshow.com>
2023-12-06 14:59:06 +0800

    测试中间退出问题

--- hive/startup.sh
+++ hive/startup.sh
@@ -9,15 +9,3 @@ hiveservices.sh start
 
 cd $HIVE_HOME/bin
 ./hiveserver2 --hiveconf hive.server2.enable.doAs=false
-sleep 60
-
-CREATE TABLE IF NOT EXISTS users (
-    user_id INT,
-    username STRING,
-    email STRING
-);
-
-INSERT INTO users VALUES
-    (1, 'user1', 'user1@example.com'),
-    (2, 'user2', 'user2@example.com'),
-    (3, 'user3', 'user3@example.com');
