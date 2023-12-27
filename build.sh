38ae6e981b56aacd821b134a99b1f2c59697de08
wantao <wantao@inmyshow.com>
2023-12-27 16:48:17 +0800

    flink单机搭建

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-TAG=1.14.5-hadoop3.2
+TAG=1.16.3_scala_2.12
 
 build() {
     NAME=$1
-    IMAGE=bde2020/flink-$NAME:$TAG
+    IMAGE=flink-$NAME:$TAG
     cd $([ -z "$2" ] && echo "./$NAME" || echo "$2")
     echo '--------------------------' building $IMAGE in $(pwd)
     docker build -t $IMAGE .
@@ -16,6 +16,6 @@ build() {
 build base
 build master
 build worker
-build submit
-build maven-template template/maven
-build sbt-template template/sbt
+#build submit
+#build maven-template template/maven
+#build sbt-template template/sbt
